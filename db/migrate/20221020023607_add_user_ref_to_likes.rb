e0ef4312fac586b83681bc81fa8e77d7b23b7e0c
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-20 03:42:25 +0100

    add user reference to coments

--- db/migrate/20221020023607_add_user_ref_to_likes.rb
+++ db/migrate/20221020023607_add_user_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :like, :author, null: false, foreign_key: { to_table: :user }
+    add_reference :likes, :author, null: false, foreign_key: { to_table: :user }
   end
 end
