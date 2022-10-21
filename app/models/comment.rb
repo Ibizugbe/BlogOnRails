e91f502023661b5f5a05585778d0b6ae7e3bc0e0
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-21 11:12:47 +0100

    add method to update comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :users
+  belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_comment
+    post.increment!(:comments_counter)
+  end
 end
