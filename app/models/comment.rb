de091813e7c64dd08bf68dae8314571dbd09457e
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-21 11:17:10 +0100

    add method to update like counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  after_save :update_comment_counter
+
   def update_comment_counter
-    post.increment!(:comments_counter)
+    post.increment!(:CommentsCounter)
   end
 end
