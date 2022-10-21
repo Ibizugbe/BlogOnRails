0cd5eff57c80361a5d817184e09f2c77d4f5baea
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-21 12:03:44 +0100

    fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   after_save :update_like_counter
-  
+
   def update_like_counter
     post.increment!(:LikesCounter)
   end
