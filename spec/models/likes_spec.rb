2dcb759f151ec499d016f15681da7f2567c70ee4
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-21 20:08:58 +0100

    add test for likes

---
+++ spec/models/likes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'ffi'
+
+RSpec.describe Like, type: :model do
+  user = User.create(Name: 'John', Photo: 'https://www.google.com/url', Bio: 'I am a software developer',
+                     PostsCounter: 3)
+  post = Post.create(Title: 'Hello', Text: 'Hello world', author_id: user.id, likes_counter: 6, CommentsCounter: 3)
+
+  like = Like.create(author_id: user.id, post_id: post.id)
+
+  context 'Update likes counter' do
+    like.update_likes_counter
+
+    it 'should increment likes counter' do
+      expect(post.likes_counter).to eq(6)
+    end
+  end
+end
