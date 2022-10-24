295dc269b45b071d5183dd0f4b4183d26f15cad9
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-24 12:17:12 +0100

    rename LikesCounter column

--- db/migrate/20221024110116_change_column_name.rb
+++ db/migrate/20221024110116_change_column_name.rb
@@ -4,7 +4,7 @@ class ChangeColumnName < ActiveRecord::Migration[7.0]
     rename_column :posts, :Title, :Title
     rename_column :posts, :Text, :text
     rename_column :posts, :CommentsCounter, :comment_counter
-    rename_column :posts, :PostCounter, :post_counter
+    rename_column :posts, :LikesCounter, :like_counter
     rename_column :users, :PostCounter, :post_counter
     rename_column :users, :Name, :name
     rename_column :users, :Photo, :photo
