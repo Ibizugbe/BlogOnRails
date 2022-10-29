92a103f2425d230de10d06d85081ade011f38436
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-29 14:08:01 +0100

    update post controller to get posts from users

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+  
+  after_create :update_post_counter
   def return_recent_comments
     Comment.limit(5).order(created_at: :desc)
   end
