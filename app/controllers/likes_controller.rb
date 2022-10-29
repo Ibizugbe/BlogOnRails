effe920b7a124d4850094eeb8f091d8118b213e1
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-29 15:44:24 +0100

    update user controller and fix application break

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     like = Like.new
-    like.user = current_user
+    like.author = current_user
     like.post = post
     if like.save
       flash[:success] = 'Liked successfully'
