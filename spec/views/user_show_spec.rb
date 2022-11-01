4601bbab5afdd0377328f0e76aef733061a7b088
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-01 07:48:53 +0100

    write test to show first three post

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe 'renders users Show Page', type: :feature do
   before :each do
     @user = User.create(name: 'Tom Ford', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                         bio: 'I am a fashion designer with 2 decades of experience', post_counter: 0)
+    
+    visit root_path
 
     5.times do |i|
       Post.create(author: @user, title: "Post #{i}", text: 'This is my post')
     end
-    visit user_path(id: @user.id)
+    visit user_path(@user.id)
   end
 
   it 'should display user name and bio' do
@@ -34,5 +36,16 @@ RSpec.describe 'renders users Show Page', type: :feature do
 
   it 'shows post comments' do
     expect(page).to have_content('I am a fashion designer with 2 decades of experience')
+  end
+
+  it "show user's first 3 posts." do
+    expect(page).to have_content 'Hello'
+  end
+
+
+
+  it "click post and redirect to that post's show page." do
+    click_link 'See all posts'
+    expect(page).to have_current_path user_posts_path(@user)
   end
 end
